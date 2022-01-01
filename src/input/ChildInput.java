9e12a90e480a749cfb20051c26697ba23bdc8d03
andreeaprigoreanu <andreea.prigoreanu@yahoo.com>
2022-01-01 18:00:47 +0200

    Add input classes

---
+++ src/input/ChildInput.java
@@ -0,0 +1,72 @@
+package input;
+
+import enums.Category;
+import enums.Cities;
+
+import java.util.ArrayList;
+
+public final class ChildInput {
+    private Integer id;
+    private String lastName;
+    private String firstName;
+    private Integer age;
+    private Cities city;
+    private Double niceScore;
+    private ArrayList<Category> giftsPreferences;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(final Integer id) {
+        this.id = id;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(final String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(final String firstName) {
+        this.firstName = firstName;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(final Integer age) {
+        this.age = age;
+    }
+
+    public Cities getCity() {
+        return city;
+    }
+
+    public void setCity(final Cities city) {
+        this.city = city;
+    }
+
+    public Double getNiceScore() {
+        return niceScore;
+    }
+
+    public void setNiceScore(final Double niceScore) {
+        this.niceScore = niceScore;
+    }
+
+    public ArrayList<Category> getGiftsPreferences() {
+        return giftsPreferences;
+    }
+
+    public void setGiftsPreferences(final ArrayList<Category> giftsPreferences) {
+        this.giftsPreferences = giftsPreferences;
+    }
+}
---
+++ src/input/InitialData.java
@@ -0,0 +1,35 @@
+package input;
+
+import enums.Cities;
+
+import java.util.ArrayList;
+
+public final class InitialData {
+    private ArrayList<ChildInput> children;
+    private ArrayList<GiftInput> santaGiftsList;
+    private ArrayList<Cities> cities;
+
+    public ArrayList<ChildInput> getChildren() {
+        return children;
+    }
+
+    public void setChildren(final ArrayList<ChildInput> children) {
+        this.children = children;
+    }
+
+    public ArrayList<GiftInput> getSantaGiftsList() {
+        return santaGiftsList;
+    }
+
+    public void setSantaGiftsList(final ArrayList<GiftInput> santaGiftsList) {
+        this.santaGiftsList = santaGiftsList;
+    }
+
+    public ArrayList<Cities> getCities() {
+        return cities;
+    }
+
+    public void setCities(final ArrayList<Cities> cities) {
+        this.cities = cities;
+    }
+}
